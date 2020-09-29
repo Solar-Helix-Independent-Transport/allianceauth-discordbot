fde5c4341918f8fbdef70003e54de7249341a644
Ariel Rin <ozirascal@gmail.com>
2020-09-30 02:33:28 +1000

    Shiny Models to use

--- aadiscordbot/admin.py
+++ aadiscordbot/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import *
 
+from allianceauth.services.hooks import get_extension_logger
 
+logger = get_extension_logger(__name__)
+
+@admin.register(Servers)
+class ServersAdmin(admin.ModelAdmin):
+    list_display = ('server_id', 'server_name')
+    ordering = ('server_id',)
+
+    search_fields = ('server_name',)
+
+@admin.register(Channels)
+class ChannelsAdmin(admin.ModelAdmin):
+    list_display = ('server_name', 'channel_id', 'channel_name')
+    ordering = ('channel_name',)
+
+    search_fields = ('channel_name',)
+
+    @staticmethod
+    def server_name(obj):
+        try:
+            return obj.server_name
+        except Exception as e:
+            logger.error(e)
--- aadiscordbot/models.py
+++ aadiscordbot/models.py
@@ -1,3 +1,40 @@
+from django.conf import settings
 from django.db import models
 
+class DiscordBot(models.Model):
+    """Meta model for app permissions"""
 
+    class Meta:
+        managed = False                         
+        default_permissions = ()
+        permissions = ( 
+            ('basic_access', 'Can access this app'), 
+        )
+
+class Servers(models.Model):
+    """Servers and their ID"""
+
+    server_id = models.PositiveBigIntegerField(primary_key=True)
+    server_name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Server'
+        verbose_name_plural = 'Servers'
+
+    def __str__(self):
+        return '{}'.format(self.server_name)
+
+class Channels(models.Model):
+    """Channel IDs, Names and the Server they belong to"""
+
+    server_id = models.ForeignKey(Servers, on_delete=models.CASCADE)
+
+    channel_id = models.PositiveBigIntegerField(primary_key=True)
+    channel_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '"{}" On "{}"'.format(self.channel_name, self.server_id.server_name)
+
+    class Meta:
+        verbose_name = 'Channel'
+        verbose_name_plural = 'Channels'
