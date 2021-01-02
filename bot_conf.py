bc274a4e06763c6c103472cebd599837200fdcb9
Ariel Rin <ozirascal@gmail.com>
2021-01-02 20:17:41 +1000

    remove click

--- bot_conf.py
+++ bot_conf.py
@@ -1,15 +1,13 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myauth.settings.local")
-
-from aadiscordbot import launcher
-import click
 import asyncio
-@click.group(invoke_without_command=True)
-@click.pass_context
+from aadiscordbot import launcher
+
+
 def main(ctx):
-    if ctx.invoked_subcommand is None:
-        loop = asyncio.get_event_loop()
-        launcher.run_bot()
+    loop = asyncio.get_event_loop()
+    launcher.run_bot()
+
 
 if __name__ == "__main__":
     main()
