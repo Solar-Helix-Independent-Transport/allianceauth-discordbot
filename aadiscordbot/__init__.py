df28914e5149c80a029e354ed53f2b0892f0fd56
AaronKable <aaronkable@gmail.com>
2021-07-02 18:29:41 +0800

    Ariel Aprooved

--- aadiscordbot/__init__.py
+++ aadiscordbot/__init__.py
@@ -1,5 +1,5 @@
 default_app_config = 'aadiscordbot.apps.AADiscordBotConfig'
 
-__version__ = '0.3.1'
+__version__ = '9000.0.1'
 __title__ = "AA Discordbot"
-__branch__ = 'dumproles'
+__branch__ = 'Over Nine Thousand!'
