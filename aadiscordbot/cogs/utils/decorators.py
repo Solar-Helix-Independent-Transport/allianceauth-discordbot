1f7069e179f08a4484258dcd491a8569ede6124a
Joel Falknau <ozirascal@gmail.com>
2021-07-10 20:05:07 +1000

    Extra decorater options

--- aadiscordbot/cogs/utils/decorators.py
+++ aadiscordbot/cogs/utils/decorators.py
@@ -12,7 +12,10 @@ logger = logging.getLogger(__name__)
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 
-def sender_has_perm(perm):
+def sender_has_perm(perm: str):
+    """
+    Permission Decorator: Does the user have x Django Permission
+    """
     def predicate(ctx):
         id = ctx.message.author.id
         if id in DISCORD_BOT_ADMIN_USER:
@@ -24,10 +27,53 @@ def sender_has_perm(perm):
             if id in DISCORD_BOT_ADMIN_USER:
                 return True
             else:
-                raise commands.MissingPermissions(["auth_roles"])
+                raise commands.MissingPermissions(["Missing Auth Permission"])
         except Exception as e:
             logger.error(e)
-            raise commands.MissingPermissions(["not_linked"])
+            raise commands.MissingPermissions(["Not Authenticated"])
+    return commands.check(predicate)
+
+
+def sender_has_all_perms(perms: list):
+    """
+    Permission Decorator: Does the user have x and y Django Permission
+    """
+    def predicate(ctx):
+        id = ctx.message.author.id
+        if id in DISCORD_BOT_ADMIN_USER:
+            return True
+        try:
+            has_perm = DiscordUser.objects.get(uid=id).user.has_perms(perms)
+            if has_perm:
+                return True
+            else:
+                raise commands.MissingPermissions(["Missing Auth Permission"])
+        except Exception as e:
+            logger.error(e)
+            raise commands.MissingPermissions(["Not Authenticated"])
+    return commands.check(predicate)
+
+
+def sender_has_any_perm(perms: list):
+    """
+    Permission Decorator: Does the user have x or y Django Permission
+    """
+    def predicate(ctx):
+        id = ctx.message.author.id
+        if id in DISCORD_BOT_ADMIN_USER:
+            return True
+        for perm in perms:
+            try:
+                has_perm = DiscordUser.objects.get(uid=id).user.has_perm(perm)
+                if has_perm:
+                    return True
+                if id in DISCORD_BOT_ADMIN_USER:
+                    return True
+                else:
+                    raise commands.MissingPermissions(["Missing Auth Permission"])
+            except Exception as e:
+                logger.error(e)
+                raise commands.MissingPermissions(["Not Authenticated"])
     return commands.check(predicate)
 
 
@@ -37,15 +83,15 @@ def sender_is_admin():
         if id in DISCORD_BOT_ADMIN_USER:
             return True
         else:
-            raise commands.MissingPermissions(["Admin Array"])
+            raise commands.MissingPermissions(["Not an Admin"])
     return commands.check(predicate)
 
 
 def message_in_channels(channels: list):
     def predicate(ctx):
-        channel = ctx.message.channel
+        channel = ctx.message.channel.id
         if channel in channels:
             return True
         else:
-            raise commands.MissingPermissions(["Channel Array"])
+            raise commands.MissingPermissions(["Not an Allowed Channel"])
     return commands.check(predicate)
