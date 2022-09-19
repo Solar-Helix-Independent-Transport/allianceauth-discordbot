2c92251c03ce0ed7bae0ebbebe3fd3dc30ddb8cc
Ariel Rin <ozirascal@gmail.com>
2022-09-19 22:22:07 +1000

    fix reminder calc

--- aadiscordbot/cogs/remind.py
+++ aadiscordbot/cogs/remind.py
@@ -3,6 +3,8 @@ import logging
 
 from discord.ext import commands
 
+from discord import AllowedMentions
+
 from django.conf import settings
 
 logger = logging.getLogger(__name__)
@@ -16,21 +18,12 @@ class Remind(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name='remind', guild_ids=[int(settings.DISCORD_GUILD_ID)])
+    @commands.slash_command(name='remind', description="Set a Reminder", guild_ids=[int(settings.DISCORD_GUILD_ID)])
     async def reminder(self, ctx, reminder: str, seconds: int=0, minutes: int=0, hours: int=0, days: int=0):
-        counter = 0
-        if days:
-            counter += days * 60 * 60 * 24
-        if hours:
-            counter += hours * 60 * 60
-        if minutes:
-            counter += minutes * 60
-        if seconds:
-            counter += seconds
-
-        await ctx.send_response(f"Alright, I will remind you about {reminder} in {seconds}s {minutes}m {hours}h {days}d.")
-        await asyncio.sleep(seconds)
-        await ctx.send_followup(f"{ctx.user.mention} Hi, you asked me to remind you about {reminder}, {seconds}s {minutes}m {hours}h {days}d ago.")
+        counter = seconds + minutes * 60 + hours * 60 * 60 + days * 60 * 60 * 24 
+        await ctx.respond(f"Alright, I will remind you about {reminder} in {seconds}s {minutes}m {hours}h {days}d.", allowed_mentions = AllowedMentions(everyone=False, roles=False, users=False))
+        await asyncio.sleep(counter)
+        await ctx.respond(f"{ctx.user.mention} Hi, you asked me to remind you about {reminder}, {seconds}s {minutes}m {hours}h {days}d ago.", allowed_mentions = AllowedMentions(everyone=False, roles=False, users=[ctx.user]))
 
 def setup(bot):
     bot.add_cog(Remind(bot))
