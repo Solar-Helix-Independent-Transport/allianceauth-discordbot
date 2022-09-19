6434b707e0414d58d4f5b42c85f6600870d66854
Ariel Rin <ozirascal@gmail.com>
2022-09-19 20:18:40 +1000

    slashify

--- aadiscordbot/cogs/remind.py
+++ aadiscordbot/cogs/remind.py
@@ -1,11 +1,9 @@
-# Cog Stuff
 import asyncio
 import logging
-from datetime import datetime
 
-from discord.colour import Color
-from discord.embeds import Embed
 from discord.ext import commands
+
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
@@ -18,45 +16,21 @@ class Remind(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True, case_insensitive=True, aliases=["remind", "remindme", "remind_me"])
-    @commands.bot_has_permissions(attach_files=True, embed_links=True)
-    async def reminder(self, ctx, time, *, reminder):
-        print(time)
-        print(reminder)
-        embed = Embed(color=0x55a7f7, timestamp=datetime.utcnow())
-        seconds = 0
-        if reminder is None:
-            # Error message
-            embed.add_field(
-                name='Warning', value='Please specify what do you want me to remind you about.')
-        if time.lower().endswith("d"):
-            seconds += int(time[:-1]) * 60 * 60 * 24
-            counter = f"{seconds // 60 // 60 // 24} days"
-        if time.lower().endswith("h"):
-            seconds += int(time[:-1]) * 60 * 60
-            counter = f"{seconds // 60 // 60} hours"
-        elif time.lower().endswith("m"):
-            seconds += int(time[:-1]) * 60
-            counter = f"{seconds // 60} minutes"
-        elif time.lower().endswith("s"):
-            seconds += int(time[:-1])
-            counter = f"{seconds} seconds"
-        if seconds == 0:
-            embed.add_field(name='Warning',
-                            value='Please specify a proper duration, send `reminder_help` for more information.')
-        elif seconds < 300:
-            embed.add_field(name='Warning',
-                            value='You have specified a too short duration!\nMinimum duration is 5 minutes.')
-        elif seconds > 7776000:
-            embed.add_field(
-                name='Warning', value='You have specified a too long duration!\nMaximum duration is 90 days.')
-        else:
-            await ctx.send(f"Alright, I will remind you about {reminder} in {counter}.")
-            await asyncio.sleep(seconds)
-            await ctx.send(f"Hi, you asked me to remind you about {reminder} {counter} ago.")
-            return
-        await ctx.send(embed=embed)
+    @commands.slash_command(name='remind', guild_ids=[int(settings.DISCORD_GUILD_ID)])
+    async def reminder(self, ctx, reminder: str, seconds: int=0, minutes: int=0, hours: int=0, days: int=0):
+        counter = 0
+        if days:
+            counter += days * 60 * 60 * 24
+        if hours:
+            counter += hours * 60 * 60
+        if minutes:
+            counter += minutes * 60
+        if seconds:
+            counter += seconds
 
+        await ctx.send_response(f"Alright, I will remind you about {reminder} in {seconds}s {minutes}m {hours}h {days}d.")
+        await asyncio.sleep(seconds)
+        await ctx.send_followup(f"{ctx.user.mention} Hi, you asked me to remind you about {reminder}, {seconds}s {minutes}m {hours}h {days}d ago.")
 
 def setup(bot):
     bot.add_cog(Remind(bot))
