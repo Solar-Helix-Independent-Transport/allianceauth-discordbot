a678fe5c48b04e0cbe5b644f0a47a1ea2af8fcfb
Aaron Kable <aaronkable@gmail.com>
2023-04-29 15:01:05 +0800

    vbump?

--- aadiscordbot/__init__.py
+++ aadiscordbot/__init__.py
@@ -1,5 +1,5 @@
 default_app_config = 'aadiscordbot.apps.AADiscordBotConfig'
 
-__version__ = '3.5.0'
+__version__ = '3.5.1b1'
 __title__ = "AA Discordbot"
-__branch__ = 'stable'
+__branch__ = 'stable?'
