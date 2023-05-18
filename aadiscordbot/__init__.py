fc53cdf3e21d52b0687f3cde4fd3af94c735f571
Aaron Kable <aaronkable@gmail.com>
2023-05-18 18:49:03 +0800

    b1wq

--- aadiscordbot/__init__.py
+++ aadiscordbot/__init__.py
@@ -1,5 +1,5 @@
 default_app_config = 'aadiscordbot.apps.AADiscordBotConfig'
 
-__version__ = '3.6.1'
+__version__ = '3.6.2b1'
 __title__ = "AA Discordbot"
-__branch__ = 'stable'
+__branch__ = 'stable?'
