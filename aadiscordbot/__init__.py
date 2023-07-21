e421a8ef56c3505705a3fcb9ea055092cc9c6213
Aaron Kable <aaronkable@gmail.com>
2023-07-21 21:41:55 +0800

    version bump

--- aadiscordbot/__init__.py
+++ aadiscordbot/__init__.py
@@ -4,6 +4,6 @@ Alliance Auth Modular Discord Bot
 
 default_app_config = 'aadiscordbot.apps.AADiscordBotConfig'
 
-__version__ = '3.6.3'
+__version__ = '3.6.4'
 __title__ = "AA Discordbot"
 __branch__ = 'stable'
