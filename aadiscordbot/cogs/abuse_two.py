1af5cd0d956d42ff46201da14fb393ea18311601
Aaron Kable <aaronkable@gmail.com>
2023-11-06 16:29:04 +0800

    move the arial abuse cog

--- aadiscordbot/cogs/abuse_two.py
+++ aadiscordbot/cogs/abuse_two.py
@@ -27,6 +27,13 @@ class AbuseTwo(commands.Cog):
         if message.author.id == settings.DISCORD_BOT_ABUSEE_ID:
             await message.add_reaction(getattr(settings, 'DISCORD_BOT_ABUSEE_EMOJI', '👎'))
 
+    if not getattr(settings, "BE_NICE_TO_ARIEL", False):
+        # Ariel is wrong, always.
+        @commands.Cog.listener("on_message")
+        async def respond_to_ariel(self, message):
+            if message.author.id == 140706470856622080:
+                await message.add_reaction('👎')
+
 
 def setup(bot):
     bot.add_cog(AbuseTwo(bot))
