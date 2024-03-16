7c48d847d49cec95fbad3150bca3618a67245a0e
Aaron Kable <aaronkable@gmail.com>
2024-03-16 13:48:36 +0800

    alpha

--- aadiscordbot/__init__.py
+++ aadiscordbot/__init__.py
@@ -4,6 +4,6 @@ Alliance Auth Modular Discord Bot
 
 default_app_config = 'aadiscordbot.apps.AADiscordBotConfig'
 
-__version__ = '3.7.2'
+__version__ = '4.0.0a1'
 __title__ = "AA Discordbot"
-__branch__ = 'stable'
+__branch__ = 'Global-Alpha'
