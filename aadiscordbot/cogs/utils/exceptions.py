f25ed922bbfce85208976d367479987b7e30e43a
Aaron Kable <aaronkable@gmail.com>
2024-03-25 17:19:24 +0800

    add new exception for unmanaged server

--- aadiscordbot/cogs/utils/exceptions.py
+++ aadiscordbot/cogs/utils/exceptions.py
@@ -14,3 +14,13 @@ class NotAuthenticated(CheckFailure):
 
         message = f"You must be an Authenticated user to run this command {get_site_url()}"
         super().__init__(message, *args)
+
+
+class NotManaged(CheckFailure):
+    """
+        Custom Exception to handle any guild not managed by AA
+    """
+
+    def __init__(self, *args: Any, dm: bool = False) -> None:
+        message = f"Command must be used in a guild managed by {get_site_url()}"
+        super().__init__(message, *args)
