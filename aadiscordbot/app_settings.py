13a1400e6334cc19f3e1bcbfd1196b33fe18b915
Aaron Kable <aaronkable@gmail.com>
2024-03-25 17:17:24 +0800

    Add recruit me cog

--- aadiscordbot/app_settings.py
+++ aadiscordbot/app_settings.py
@@ -121,3 +121,17 @@ DISCORD_BOT_FAILURE_MESSAGES_CHANNEL = getattr(
 
 PRICE_CHECK_HOSTNAME = getattr(
     settings, 'PRICE_CHECK_HOSTNAME', "evepraisal.com")
+
+// List of ints to sync commands
+DISCORD_GUILD_IDS = getattr(settings, 'DISCORD_GUILD_IDS', [])
+
+DISCORD_GUILD_ID = getattr(settings, 'DISCORD_GUILD_ID', None)
+
+
+def get_all_servers():
+    servers = []
+    if DISCORD_GUILD_IDS:
+        servers += DISCORD_GUILD_IDS
+    if DISCORD_GUILD_ID and DISCORD_GUILD_ID not in servers:
+        servers.append(int(DISCORD_GUILD_ID))
+    return servers
