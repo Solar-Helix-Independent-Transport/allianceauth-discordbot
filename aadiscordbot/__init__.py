ed7c60490ef751dbd351c157696a5be98e0478d6
Aaron Kable <aaronkable@gmail.com>
2024-05-25 14:03:28 +0800

    stable not master

--- aadiscordbot/__init__.py
+++ aadiscordbot/__init__.py
@@ -2,8 +2,7 @@
 Alliance Auth Modular Discord Bot
 """
 
-default_app_config = 'aadiscordbot.apps.AADiscordBotConfig'
 
 __version__ = '4.0.0b1'
 __title__ = "AA Discordbot"
-__branch__ = 'master'
+__branch__ = 'stable'
