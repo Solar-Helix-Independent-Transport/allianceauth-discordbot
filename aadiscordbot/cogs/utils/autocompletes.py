e100d8c377b3f1c94ecb21b09e089969ff30cb23
Aaron Kable <aaronkable@gmail.com>
2024-10-13 15:30:27 +0800

    remove more async ORM

--- aadiscordbot/cogs/utils/autocompletes.py
+++ aadiscordbot/cogs/utils/autocompletes.py
@@ -6,7 +6,7 @@ from allianceauth.eveonline.models import EveCharacter
 async def search_characters(ctx: AutocompleteContext):
     """Returns a list of colors that begin with the characters entered so far."""
     return [
-        a async for a in EveCharacter.objects.filter(
+        a for a in EveCharacter.objects.filter(
             character_name__icontains=ctx.value
         ).values_list(
             'character_name',
